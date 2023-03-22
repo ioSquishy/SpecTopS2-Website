1431c5e2ab81b7262b5463441fa5d348a9f15937
ioSquishy <squishylxix@gmail.com>
2023-03-22 12:25:12 -0700

    saved form in js

--- JavaScripts/form.js
+++
@@ -1,3 +0,0 @@
-let submitBtn = document.getElementById("submitBtn");
-let nameField = document.getElementById("nameField");
-let title = document.getElementById("title");
---
+++ Scripts/form.js
@@ -0,0 +1,9 @@
+function submitForm() {
+  var firstName = document.getElementById("fName").value;
+  var maidenName = document.getElementById("mName").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("number").value;
+  var comments = document.getElementById("comments").value;
+
+  
+}
