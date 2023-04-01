366c872991d301621e59e908b31b30a72fdcb2cd
ArthurLVDH <squishylxix@gmail.com>
2023-04-01 13:07:28 -0700

    used sheetdb api and it works

--- Scripts/form.js
+++ Scripts/form.js
@@ -1,9 +1,14 @@
-function submitForm() {
-  var firstName = document.getElementById("fName").value;
-  var maidenName = document.getElementById("mName").value;
-  var email = document.getElementById("email").value;
-  var phone = document.getElementById("number").value;
-  var comments = document.getElementById("comments").value;
+const form = document.getElementById('reservationForm');
 
-  
-}
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  fetch(form.action, {
+      method : "POST",
+      body: new FormData(document.getElementById("reservationForm")),
+  }).then(
+      response => response.json()
+  ).then((html) => {
+    // you can put any JS code here
+    alert('success')
+  });
+});
