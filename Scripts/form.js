0c5e4ed4e34a28d8632621d8599fa64bfefd8b88
ioSquishy <squishylxix@gmail.com>
2023-04-01 17:26:41 -0700

    checks form before submission

--- Scripts/form.js
+++ Scripts/form.js
@@ -1,19 +1,65 @@
-const form = document.getElementById('reservationForm');
 const formContainer = document.getElementById('form-container');
 const reservationsContainer = document.getElementById('reservations-container');
 const successTemplate = document.getElementsByTagName("template")[0];
 
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  fetch(form.action, {
-      method : "POST",
-      body: new FormData(document.getElementById("reservationForm")),
-  }).then(
-      response => response.json()
-  ).then((html) => {
-    // you can put any JS code here
+const validateEmail = (email) => {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+};
+
+function checkForm() {
+  var firstName = document.getElementById("fName").value;
+  var maidenName = document.getElementById("mName").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("number").value;
+  var comments = document.getElementById("comments").value;
+
+  if (firstName && maidenName && validateEmail(email) && phone) {
+    submitForm(firstName, maidenName, email, phone, comments);
     formContainer.style.display = "none";
     let template = successTemplate.content.cloneNode(true);
     reservationsContainer.appendChild(template);
-  });
-});
+  } else {
+    alert("Make sure all fields except Allergies/Comments are filled and that you have a valid email address!");
+  }
+}
+
+function submitForm(firstName, maidenName, email, phone, comments) {
+  fetch('https://sheetdb.io/api/v1/tfs4ekwkf7ny6', {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        data: [
+            {
+                'First Name': firstName,
+                'Maiden Name': maidenName,
+                'Email': email,
+                'Phone' : phone,
+                'Comments' : comments
+            }
+        ]
+    })
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+}
+
+function testSubmit() {
+  var firstName = document.getElementById("fName").value;
+  var maidenName = document.getElementById("mName").value;
+  var email = document.getElementById("email").value;
+  var phone = document.getElementById("number").value;
+  var comments = document.getElementById("comments").value;
+
+  if (firstName && maidenName && validateEmail(email) && phone) {
+    formContainer.style.display = "none";
+    let template = successTemplate.content.cloneNode(true);
+    reservationsContainer.appendChild(template);
+  } else {
+    alert(":(");
+  }
+}
